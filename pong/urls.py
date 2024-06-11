441a742007efe53cbb4d79fbbfa1197d08dfb005
puyma <mpuig-ma@student.42barcelona.com>
2024-06-11 16:48:14 +0200

    renamed: app to pong

--- pong/urls.py
+++ pong/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for app project.
+URL configuration for pong project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
 
 urlpatterns = [
-    path( "polls/", include( "polls.urls" ) ),
-    path( "admin/", admin.site.urls ),
+    path('admin/', admin.site.urls),
 ]
