35a089675aeae4114d013ba5003aed17cc6f8c2d
puyma <mpuig-ma@student.42barcelona.com>
2024-06-17 16:23:16 +0200

    restructured project layout

--- transcendence/hello_world/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-# Create your views here.
-def say_hello(request) :
-	context = {}
-	return render(request, "hello_world/index.html", context)
