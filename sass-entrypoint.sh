ef607ae56912e5488f69a6a465a7ebc88eea33dd
puyma <mpuig-ma@student.42barcelona.com>
2024-09-02 17:56:31 +0200

    add sass container; use bootstrap package

---
+++ sass-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+alias sass="./node_modules/sass/sass.js" 
+
+IN="scss/main.scss"
+OUT="static/css/style.css"
+
+if test "$DEBUG" = "True"
+then
+	sass --watch "$IN" "$OUT"
+else
+	sass "$IN" "$OUT"
+fi
