639036db2380d59429f8b267c772025e238ffccd
puyma <mpuig-ma@student.42barcelona.com>
2024-09-04 16:13:50 +0200

    fix: cd

--- assets-entrypoint.sh
+++ assets-entrypoint.sh
@@ -2,6 +2,13 @@
 
 set -x
 
+for item in assets/*
+do
+	mkdir -p "static/`dirname $item`"
+	cp --archive "$item" "static/assets/"
+	echo "> $item"
+done
+
 FONT_URL="https://fonts.gstatic.com/s/dotgothic16/v18/v6-QGYjBJFKgyw5nSoDAGH7M6X8.woff2"
 FILE="static/fonts/dotgothic16/v18/v6-QGYjBJFKgyw5nSoDAGH7M6X8.woff2"
 
@@ -11,9 +18,3 @@ then
 	cd "static/fonts/dotgothic16/v18/"
 	wget -O "`basename $FILE`" "$FONT_URL"
 fi
-
-for item in ./assets/*
-do
-	mkdir -p "static/`dirname $item`"
-	cp --recursive --archive "$item" "static/assets/"
-done
