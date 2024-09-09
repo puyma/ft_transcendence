96ab7d600843a9c23cf85d1fb21b18b36aefeeef
puyma <mpuig-ma@student.42barcelona.com>
2024-09-09 13:16:24 +0200

    use terser
    use source-maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,42 @@
-const path = require( 'path' );
 const debug = ( String( process.env.DEBUG ).toLowerCase() === "true" ) ? true : false
+
+const path = require( 'path' );
+const TerserPlugin = require( 'terser-webpack-plugin' );
 
 module.exports = {
 	mode: ( debug == true ) ? "development" : "production",
+	devtool: 'source-map',
 	entry: './js/main.js',
 	output: {
 			path: path.resolve( __dirname, 'static/js' ),
 			filename: 'bundle.js',
+	},
+	optimization: {
+		concatenateModules: true,
+		minimizer: [
+			new TerserPlugin( {
+				parallel: true, terserOptions: {
+					compress: {
+						defaults: true
+					},
+					ecma: undefined,
+					enclose: false,
+					ie8: false,
+					keep_classnames: true,
+					keep_fnames: true,
+					mangle: false,
+					module: false,
+					nameCache: null,
+					//sourceMap: {},
+					format: {
+						braces: true,
+						comments: false,
+						semicolons: true,
+						preserve_annotations: false
+					},
+				}
+			} )
+		]
 	},
 	watch: ( debug == true ) ? true : false,
 }
