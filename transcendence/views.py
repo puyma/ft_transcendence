09e67255ac3986f8a96529569bc6e1addaa468d3
Martí <mpuig-ma@student.42barcelona.com>
2024-09-25 13:21:57 +0200

    merge: 14-login-using-djangocontribauth (#24)
    * remove redundant action in Makefile
    
    * fix: proper use of variable interpolation intest cmd
    
    * changed button for anchor
    
    * login test
    
    * init login page
    
    * rm unused files
    
    * add sass container; use bootstrap package
    
    * load font from local
    
    * fix: font load url
    
    * fclean: remove node_modules
    
    * fix: font path
    
    modify: container_name
    
    * splited header
    
    * fix: improper replaced file
    
    * fix: assets
    
    readded: thumbnail.png
    
    * fix: path
    
    * separar tag head
    
    * fix: cd
    
    * add: down stop
    
    * rm: not needed anymore
    
    * mv: urls.py
    
    * use example header
    
    * add: make debug; attach mode
    
    * proper debug env
    
    * proper debug env
    
    * proper debug env
    
    * GET logout/
    
    Profile model
    
    * added profile picture
    
    * so far
    
    * update README
    
    * mv dir
    
    * use terser
    
    use source-maps
    
    * wip: init nginx
    
    * minor template changes
    
    * fix: yaml indentation
    
    * implements https
    
    * update
    
    * development server
    
    * proper 80/443 8000/8443 relation with privileges
    
    * rm: comment
    
    * TRUSTED_ORIGINS debug mode
    
    * add 42api env variables
    
    * mini rewrite main.js
    
    * test redirect_uri
    
    * ready for oauth/callback
    
    * wip
    
    * ~ | Update README
    
    * - | Removed unused files
    
    * ~ | Removed app; only transcendence
    
    * ~ | Caddyfile for specific prod domain
    
    * ~ | Added some container dependencies
    
    * ~ | Added things to (maybe) do
    
    * + | Added package init file
    
    * ~ | Add location for favicon.ico
    
    * ~ | Restructured providers
    
    * + | Update README
    
    * ~ | Fix get_token; using requests library
    
    * ~ | Extract access token
    
    * ~ | Makemigrations transcendence
    
    * ~ | Add avatar href
    
    * ~ | Set post_save signals for Profile
    
    * ~ | Implement AuthBackend42
    
    * ~ | Renamed server service to nginx
    
    * ~ | Roadmap urls
    
    * ~ | Renamed app directory to tr
    
    * ~ | Fix components path
    
    * ~ | Removed unused script tag
    
    * ~ | Fix component path
    
    * ~ | Implement delete account

--- transcendence/views.py
+++ transcendence/views.py
@@ -1,53 +1,153 @@
+from django import urls
+from django.contrib import auth
+from django.contrib import messages
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.views.generic.base import TemplateView
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views import generic
 
-class HomepageView ( TemplateView ):
-	template_name = "home.html"
+from . import forms
+from . import models
+from .providers import fortytwo
+
+# https://django-advanced-training.readthedocs.io/en/latest/features/class-based-views/
+
+class HomepageView ( generic.TemplateView ):
+	template_name = "tr/base.html"
 
 	def get_context_data ( self, **kwargs ):
 		context = super().get_context_data( **kwargs )
-		context["title"] = "P4ngP2ong"
-		context["lang"] = "en"
+		context["page"] = "tr/pages/home.html"
 		return ( context )
 
-class TournamentView ( TemplateView ):
-	template_name = "tournament.html"
+class LoginView ( auth_views.LoginView ):
+	redirect_authenticated_user = True
+	template_name = "tr/base.html"
+	# settings.py: next_page = "/profile"
 
 	def get_context_data ( self, **kwargs ):
 		context = super().get_context_data( **kwargs )
-		context["title"] = "P4ngP2ong"
-		context["lang"] = "en"
+		context["page"] = "tr/pages/login.html"
+		context["form"] = forms.LoginForm()
+		context['provider_42_login'] = fortytwo.get_login_url( "42", 
+				{"state": self.request.COOKIES.get( 'csrftoken' ) } )
 		return ( context )
+
+@login_required
+def do_logout ( request ):
+	auth.logout( request )
+	messages.success( request, "proper logout" )
+	return redirect( 'home' )
+
+class LogoutView ( auth_views.LogoutView ):
+	http_method_names = ["post", "get"]
+	template_name = "tr/base.html"
+
+	def get_context_data ( self, **kwargs ):
+		context = super().get_context_data( **kwargs )
+		context["page"] = "tr/pages/logout.html"
+		return ( context )
+
+	def get ( self, request, *args, **kwargs ):
+		auth.logout( request )
+		redirect_to = self.get_success_url()
+		if redirect_to != request.get_full_path():
+			return ( HttpResponseRedirect( redirect_to ) )
+		return ( super().get( request, *args, **kwargs ) )
+
+class SignupView ( generic.CreateView ):
+  form_class = auth.forms.UserCreationForm
+  success_url = urls.reverse_lazy( 'login' )
+  template_name = "tr/base.html"
+
+  def get_context_data ( self, **kwargs ):
+	  context = super().get_context_data( **kwargs )
+	  context["page"] = "tr/pages/signup.html"
+	  context["form"] = forms.SignupForm()
+	  return ( context )
+
+@login_required
+def profile_dashboard ( request ):
+	return ( render( request, 'tr/pages/base.html', {'page': 'tr/pages/profile.html'} ) )
+
+class ProfileView ( generic.TemplateView ):
+	template_name = "tr/base.html"
+
+	def get_context_data ( self, **kwargs ):
+		context = super().get_context_data( **kwargs )
+		context["page"] = "tr/pages/profile.html"
+		return ( context )
+
+@login_required
+def profile_delete ( request ):
+	user_pk = request.user.pk
+	auth.logout( request )
+	User = auth.get_user_model()
+	User.objects.filter( pk=user_pk ).delete()
+	messages.success( request, "account delete successfully" )
+	return ( redirect( 'home' ) )
+
+class TournamentView ( generic.TemplateView ):
+	template_name = "tr/base.html"
+
+	def get_context_data ( self, **kwargs ):
+		context = super().get_context_data( **kwargs )
+		context["page"] = "tr/pages/tournament.html"
+		return ( context )
+
+class PlayView ( generic.TemplateView ):
+	template_name = "tr/base.html"
+
+	def get_context_data ( self, **kwargs ):
+		context = super().get_context_data( **kwargs )
+		context["page"] = "tr/pages/tournament.html"
+		return ( context )
+
+class GameView ( generic.TemplateView ):
+	template_name = "tr/pages/pong.html"
 	
-def double_play_view(request):
-    context = {
-		"title":"P4ngP2ong",
-		"lang":"en",
-        "username": "clara"
-    }
-    return render(request, 'double_play.html', context)
+	def get_context_data ( self, **kwargs ):
+		context = super().get_context_data( **kwargs )
+		context["page"] = "tr/pages/pong.html"
+		return ( context )
 
-def solo_play_view(request):
-    context = {
-		"title":"P4ngP2ong",
-		"lang":"en",
-        "username": "clara"
-    }
-    return render(request, 'solo_play.html', context)
+##
 
-def play_view(request):
+def double_play_view ( request ):
     context = {
 		"title":"P4ngP2ong",
 		"lang":"en",
-        "username": "clara"
+        "username": "clara",
+		"page": "tr/pages/double_play.html",
     }
-    return render(request, 'play.html', context)
+    return ( render( request, 'tr/base.html', context) )
 
-def pong_view(request):
+def solo_play_view ( request ):
+
+	context = {
+		"title":"P4ngP2ong",
+		"lang":"en",
+        "username": "clara",
+		"page": "tr/pages/solo_play.html",
+    }
+	return ( render( request, 'tr/base.html', context ) )
+
+def play_view ( request ):
     context = {
 		"title":"P4ngP2ong",
 		"lang":"en",
-        "username": "clara"
+        "username": "clara",
+		"page": "tr/pages/play.html",
     }
-    return render(request, 'pong.html', context)
+    return ( render ( request, 'tr/base.html', context ) )
+
+def pong_view ( request ):
+    context = {
+		"title":"P4ngP2ong",
+		"lang":"en",
+        "username": "clara",
+		"page": "tr/pages/pong.html",
+    }
+    return ( render( request, 'tr/base.html', context ) )
