9df9d701d1e31265d0109d583fa532c5af6f8f97
Martí <mpuig-ma@student.42barcelona.com>
2024-09-25 13:21:57 +0200

    merge: 14-login-using-djangocontribauth (#24)
    * remove redundant action in Makefile
    
    * fix: proper use of variable interpolation intest cmd
    
    * changed button for anchor
    
    * login test
    
    * init login page
    
    * rm unused files
    
    * add sass container; use bootstrap package
    
    * load font from local
    
    * fix: font load url
    
    * fclean: remove node_modules
    
    * fix: font path
    
    modify: container_name
    
    * splited header
    
    * fix: improper replaced file
    
    * fix: assets
    
    readded: thumbnail.png
    
    * fix: path
    
    * separar tag head
    
    * fix: cd
    
    * add: down stop
    
    * rm: not needed anymore
    
    * mv: urls.py
    
    * use example header
    
    * add: make debug; attach mode
    
    * proper debug env
    
    * proper debug env
    
    * proper debug env
    
    * GET logout/
    
    Profile model
    
    * added profile picture
    
    * so far
    
    * update README
    
    * mv dir
    
    * use terser
    
    use source-maps
    
    * wip: init nginx
    
    * minor template changes
    
    * fix: yaml indentation
    
    * implements https
    
    * update
    
    * development server
    
    * proper 80/443 8000/8443 relation with privileges
    
    * rm: comment
    
    * TRUSTED_ORIGINS debug mode
    
    * add 42api env variables
    
    * mini rewrite main.js
    
    * test redirect_uri
    
    * ready for oauth/callback
    
    * wip
    
    * ~ | Update README
    
    * - | Removed unused files
    
    * ~ | Removed app; only transcendence
    
    * ~ | Caddyfile for specific prod domain
    
    * ~ | Added some container dependencies
    
    * ~ | Added things to (maybe) do
    
    * + | Added package init file
    
    * ~ | Add location for favicon.ico
    
    * ~ | Restructured providers
    
    * + | Update README
    
    * ~ | Fix get_token; using requests library
    
    * ~ | Extract access token
    
    * ~ | Makemigrations transcendence
    
    * ~ | Add avatar href
    
    * ~ | Set post_save signals for Profile
    
    * ~ | Implement AuthBackend42
    
    * ~ | Renamed server service to nginx
    
    * ~ | Roadmap urls
    
    * ~ | Renamed app directory to tr
    
    * ~ | Fix components path
    
    * ~ | Removed unused script tag
    
    * ~ | Fix component path
    
    * ~ | Implement delete account

--- transcendence/asgi.py
+++ transcendence/asgi.py
@@ -1,13 +1,10 @@
-# ASGI config for transcendence project.
-
 import os
-from django.core.asgi import get_asgi_application
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 from channels.routing import URLRouter
+from django.core.asgi import get_asgi_application
 from django.urls import re_path
-#from users.consumers import EchoConsumer
-#from chat.consumers import ChatConsumer
 
 os.environ.setdefault( 'DJANGO_SETTINGS_MODULE', 'transcendence.settings' )
 
@@ -18,6 +15,3 @@ application = ProtocolTypeRouter({
 			])
 		),
 	})
-
-			#re_path( r"^ws/echo/$", EchoConsumer.as_asgi() ),
-			#re_path( r"^ws/chat/$", ChatConsumer.as_asgi() ),
--- transcendence/wsgi.py
+++ transcendence/wsgi.py
@@ -1,6 +1,5 @@
-# WSGI config for transcendence project.
-
 import os
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendence.settings')
