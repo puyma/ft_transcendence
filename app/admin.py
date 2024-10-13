9b7c48d9d9576fe7c0fc7dbdbb3a3d1a5b41512b
arebelo <anagmrebelo@hotmail.com>
2024-10-13 23:47:42 +0200

    Add Match Model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Match
 
-# Register your models here.
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('winner_username', 'loser_username', 'winner_points', 'loser_points')
+    fields = ('winner_username', 'loser_username', 'winner_points', 'loser_points')
+
+admin.site.register(Match, MatchAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone 
 
-# Create your models here.
+class Match(models.Model):
+    id = models.AutoField(primary_key=True)
+    winner_username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='won_matches', null=True)
+    loser_username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lost_matches', null=True)
+    winner_points = models.IntegerField(default=0)
+    loser_points = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Match: {self.winner_username} vs {self.loser_username} on {self.created_at}"
