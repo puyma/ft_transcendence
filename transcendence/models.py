835f6ba9622102988d6ecb17363d0758fbb3cf09
puyma <mpuig-ma@student.42barcelona.com>
2024-10-24 20:25:10 +0200

    so far

--- transcendence/models.py
+++ transcendence/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db.models import signals
 from django.dispatch import receiver
 from PIL import Image
-from django.utils import timezone 
+from django.utils import timezone
 
 # Extends django.contrib.auth User
 # class Profile ( models.Model ):
@@ -13,7 +13,7 @@ from django.utils import timezone
 # 			settings.AUTH_USER_MODEL,
 # 			on_delete=models.CASCADE,
 # 			)
-			
+
 # 	avatar = models.ImageField(
 # 			upload_to='avatars/',
 # 			blank=True,
@@ -57,14 +57,17 @@ from django.utils import timezone
 # 	def __str__ ( self ):
 # 		return ( f'Profile of {self.user.username}' )
 
-#we have the profile class that is linked with the USER class (table) and to access to the first name, last name and email with
+# we have the profile class that is linked with the USER class (table) and to access to the first name, last name and email with
 # property --- print(profile.first_name)  # This will return profile.user.first_name
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(default='profile_images/default.jpg', upload_to='profile_images')
+    avatar = models.ImageField(
+        default="profile_images/default.jpg", upload_to="profile_images"
+    )
     avatar_url = models.URLField(max_length=500, blank=True, null=True)
-    bio = models.TextField( null=True )
+    bio = models.TextField(null=True)
 
     def __str__(self):
         return self.user.username
@@ -80,59 +83,66 @@ class Profile(models.Model):
     @property
     def email(self):
         return self.user.email
-    
+
     @property
     def avatar_image(self):
-        if self.avatar and self.avatar.name != 'profile_images/default.jpg':
+        if self.avatar and self.avatar.name != "profile_images/default.jpg":
             return self.avatar.url
         if self.avatar_url:
             return self.avatar_url
-        return '/media/profile_images/default.jpg'
-
+        return "/media/profile_images/default.jpg"
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-    
+
     # def save(self, *args, **kwargs):
     #     super().save(*args, **kwargs)
 
-        # Only process if the avatar is not using the default path
-        # if self.avatar and self.avatar.name != 'profile_images/default.jpg':
-        #     img = Image.open(self.avatar.path)
-        #     if img.height > 100 or img.width > 100:
-        #         new_img = (100, 100)
-        #         img.thumbnail(new_img)
-        #         img.save(self.avatar.path)
+    # Only process if the avatar is not using the default path
+    # if self.avatar and self.avatar.name != 'profile_images/default.jpg':
+    #     img = Image.open(self.avatar.path)
+    #     if img.height > 100 or img.width > 100:
+    #         new_img = (100, 100)
+    #         img.thumbnail(new_img)
+    #         img.save(self.avatar.path)
 
-@receiver( signals.post_save, sender=User )
-def create_profile__user ( sender, instance, created, **kwargs ):
-	if created:
-		Profile.objects.create( user=instance )
 
-@receiver( signals.post_save, sender=User )
-def save_profile__user ( sender, instance, update_fields, **kwargs ):
-	instance.profile.save()
+@receiver(signals.post_save, sender=User)
+def create_profile__user(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
-## fins aqui entenc be 
+
+@receiver(signals.post_save, sender=User)
+def save_profile__user(sender, instance, update_fields, **kwargs):
+    instance.profile.save()
+
+
+## fins aqui entenc be
 # @receiver( signals.post_save, sender=Profile )
 # def create_profile__profile ( sender, instance, created, update_fields, **kwargs ):
 # 	print( f"created: {created}" )
 # 	print( f"update_fields: {update_fields}\n" )
 # 	#update img; if url -> set avatar
 
+
 # @receiver( signals.post_save, sender=Profile )
 # def save_profile__profile ( sender, instance, update_fields, **kwargs ):
 # 	return
-@receiver( signals.post_save, sender=Profile )
-def save_profile__profile ( sender, instance, update_fields, **kwargs ):
-	return
+@receiver(signals.post_save, sender=Profile)
+def save_profile__profile(sender, instance, update_fields, **kwargs):
+    return
 
 
-class Match ( models.Model ):
+class Match(models.Model):
 
     id = models.AutoField(primary_key=True)
-    winner_username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='won_matches', null=True)
-    loser_username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lost_matches', null=True)
+    winner_username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="won_matches", null=True
+    )
+    loser_username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="lost_matches", null=True
+    )
     winner_points = models.IntegerField(default=0)
     loser_points = models.IntegerField(default=0)
     created_at = models.DateTimeField(default=timezone.now)
--- transcendence/views.py
+++ transcendence/views.py
@@ -3,27 +3,27 @@ from django.contrib import auth
 from django.contrib import messages
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.views import generic
-from .forms import UpdateUserForm, UpdateProfileForm
-from . import forms
+from .forms import LoginForm
+from .forms import SignupForm
+from .forms import UpdateUserForm
+from .forms import UpdateProfileForm
 from .providers import fortytwo
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
 
 
-# https://django-advanced-training.readthedocs.io/en/latest/features/class-based-views/
+class HomepageView(generic.TemplateView):
+    template_name = "tr/base.html"
 
-class HomepageView ( generic.TemplateView ):
-	template_name = 'tr/base.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "tr/pages/home.html"
+        context["active_nav"] = "home"
+        return context
 
-	def get_context_data ( self, **kwargs ):
-		context = super().get_context_data( **kwargs )
-		context['page'] = 'tr/pages/home.html'
-		context['active_nav'] = 'home'
-		return ( context )
 
 class LoginView(auth_views.LoginView):
     redirect_authenticated_user = True
@@ -32,50 +32,57 @@ class LoginView(auth_views.LoginView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["page"] = "tr/pages/login.html"
-        context["form"] = forms.LoginForm()
-        context['provider_42_login'] = fortytwo.get_login_url(
-            "42", {"state": self.request.COOKIES.get('csrftoken')}
-        )
+        context["form"] = LoginForm()
+        context["provider_42_login"] = fortytwo.get_login_url(
+                "42", {"state": self.request.COOKIES.get("csrftoken")}
+                )
         return context
 
+
+# logout should be a POST, not a GET
 @login_required
-def do_logout ( request ):
-	auth.logout( request )
-	messages.success( request, "proper logout" )
-	return redirect( 'home' )
+def do_logout(request):
+    auth.logout(request)
+    messages.success(request, "proper logout")
+    return redirect("home")
 
-class LogoutView ( auth_views.LogoutView ):
-	http_method_names = ["post", "get"]
-	template_name = 'tr/base.html'
 
-	def get_context_data ( self, **kwargs ):
-		context = super().get_context_data( **kwargs )
-		context['page'] = "tr/pages/logout.html"
-		return ( context )
+class LogoutView(auth_views.LogoutView):
+    http_method_names = ["post", "get"]
+    template_name = "tr/base.html"
 
-	def get ( self, request, *args, **kwargs ):
-		auth.logout( request )
-		redirect_to = self.get_success_url()
-		if redirect_to != request.get_full_path():
-			return ( HttpResponseRedirect( redirect_to ) )
-		return ( super().get( request, *args, **kwargs ) )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "tr/pages/logout.html"
+        return context
+
+    def get(self, request, *args, **kwargs):
+        auth.logout(request)
+        redirect_to = self.get_success_url()
+        if redirect_to != request.get_full_path():
+            return HttpResponseRedirect(redirect_to)
+        return super().get(request, *args, **kwargs)
+
 
 class SignupView(generic.CreateView):
     form_class = auth.forms.UserCreationForm
-    success_url = urls.reverse_lazy('login')
+    success_url = urls.reverse_lazy("login")
+    object = auth.models.User
     template_name = "tr/base.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["page"] = "tr/pages/signup.html"
-        context["form"] = forms.SignupForm()
+        context["form"] = SignupForm()
         return context
 
-@login_required
-def profile_dashboard ( request ):
-	return ( render( request, 'tr/pages/base.html', {'page': 'tr/pages/profile.html'} ) )
 
-class ProfileView(LoginRequiredMixin, TemplateView):
+@login_required
+def profile_dashboard(request):
+    return render(request, "tr/pages/base.html", {"page": "tr/pages/profile.html"})
+
+
+class ProfileView(LoginRequiredMixin, generic.TemplateView):
     template_name = "tr/base.html"
 
     def get_context_data(self, **kwargs):
@@ -87,17 +94,19 @@ class ProfileView(LoginRequiredMixin, TemplateView):
 
     def post(self, request, *args, **kwargs):
         user_form = UpdateUserForm(request.POST, instance=request.user)
-        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        profile_form = UpdateProfileForm(
+                request.POST, request.FILES, instance=request.user.profile
+                )
 
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
             # if profile_form.cleaned_data['avatar']: keep this lines till is all good he avatar tests !!
             #     profile = request.user.profile
-                # print("Uploaded Avatar File:", profile_form.cleaned_data['avatar'])
-                # print(" New Avatar URL:", profile.avatar.url)
-            messages.success(request, 'Your profile is updated successfully')
-            return redirect('profile')
+            # print("Uploaded Avatar File:", profile_form.cleaned_data['avatar'])
+            # print(" New Avatar URL:", profile.avatar.url)
+            messages.success(request, "Your profile is updated successfully")
+            return redirect("profile")
 
         context = self.get_context_data()
         context["user_form"] = user_form
@@ -106,121 +115,135 @@ class ProfileView(LoginRequiredMixin, TemplateView):
 
 
 @login_required
-def profile_delete ( request ):
-	user_pk = request.user.pk
-	auth.logout( request )
-	User = auth.get_user_model()
-	User.objects.filter( pk=user_pk ).delete()
-	messages.success( request, "account delete successfully" )
-	return ( redirect( 'home' ) )
+def profile_delete(request):
+    user_pk = request.user.pk
+    auth.logout(request)
+    User = auth.get_user_model()
+    User.objects.filter(pk=user_pk).delete()
+    messages.success(request, "account delete successfully")
+    return redirect("home")
 
-class PasswordChangeView ( auth_views.PasswordChangeView ):
-	template_name = 'tr/base.html'
 
-	def get_context_data ( self, **kwargs ):
-		context = super().get_context_data( **kwargs )
-		context['page'] = 'tr/pages/password_change.html'
-		return ( context )
+class PasswordChangeView(auth_views.PasswordChangeView):
+    template_name = "tr/base.html"
 
-class PasswordChangeDoneView ( auth_views.PasswordChangeDoneView ):
-	template_name = 'tr/base.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "tr/pages/password_change.html"
+        return context
 
-	def get_context_data ( self, **kwargs ):
-		context = super().get_context_data( **kwargs )
-		context['page'] = 'tr/pages/password_change_done.html'
-		return ( context )
 
-class PasswordResetView ( auth_views.PasswordResetView ):
-	template_name = 'tr/base.html'
+class PasswordChangeDoneView(auth_views.PasswordChangeDoneView):
+    template_name = "tr/base.html"
 
-	def get_context_data ( self, **kwargs ):
-		context = super().get_context_data( **kwargs )
-		context['page'] = 'tr/pages/password_reset.html'
-		return ( context )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "tr/pages/password_change_done.html"
+        return context
 
-class PasswordResetDoneView ( auth_views.PasswordResetDoneView ):
-	template_name = 'tr/base.html'
 
-	def get_context_data ( self, **kwargs ):
-		context = super().get_context_data( **kwargs )
-		context['page'] = 'tr/pages/password_reset_done.html'
-		return ( context )
+class PasswordResetView(auth_views.PasswordResetView):
+    template_name = "tr/base.html"
 
-class PasswordResetConfirmView ( auth_views.PasswordResetConfirmView ):
-	template_name = 'tr/base.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "tr/pages/password_reset.html"
+        return context
 
-	def get_context_data ( self, **kwargs ):
-		context = super().get_context_data( **kwargs )
-		context['page'] = 'tr/pages/password_reset_confirm.html'
-		return ( context )
 
-class PasswordResetCompleteView ( auth_views.PasswordResetCompleteView ):
-	template_name = 'tr/base.html'
+class PasswordResetDoneView(auth_views.PasswordResetDoneView):
+    template_name = "tr/base.html"
 
-	def get_context_data ( self, **kwargs ):
-		context = super().get_context_data( **kwargs )
-		context['page'] = 'tr/pages/password_reset_complete.html'
-		return ( context )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "tr/pages/password_reset_done.html"
+        return context
+
+
+class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+    template_name = "tr/base.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "tr/pages/password_reset_confirm.html"
+        return context
+
+
+class PasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+    template_name = "tr/base.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "tr/pages/password_reset_complete.html"
+        return context
+
 
 ## LEGACY CODE ---- ##
 
-class TournamentView ( generic.TemplateView ):
-	template_name = 'tr/base.html'
 
-	def get_context_data ( self, **kwargs ):
-		context = super().get_context_data( **kwargs )
-		context['page'] = 'tr/pages/tournament.html'
-		return ( context )
+class TournamentView(generic.TemplateView):
+    template_name = "tr/base.html"
 
-class PlayView ( generic.TemplateView ):
-	template_name = 'tr/base.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "tr/pages/tournament.html"
+        return context
 
-	def get_context_data ( self, **kwargs ):
-		context = super().get_context_data( **kwargs )
-		context['page'] = 'tr/pages/tournament.html'
-		return ( context )
 
-class GameView ( generic.TemplateView ):
-	template_name = 'tr/pages/pong.html'
-	
-	def get_context_data ( self, **kwargs ):
-		context = super().get_context_data( **kwargs )
-		context['page'] = 'tr/pages/pong.html'
-		return ( context )
+class PlayView(generic.TemplateView):
+    template_name = "tr/base.html"
 
-def double_play_view ( request ):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "tr/pages/tournament.html"
+        return context
+
+
+class GameView(generic.TemplateView):
+    template_name = "tr/pages/pong.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "tr/pages/pong.html"
+        return context
+
+
+def double_play_view(request):
     context = {
-		"title":"P4ngP2ong",
-		"lang":"en",
-        "username": "clara",
-		'page': 'tr/pages/double_play.html',
-    }
-    return ( render( request, 'tr/base.html', context) )
+            "title": "P4ngP2ong",
+            "lang": "en",
+            "username": "clara",
+            "page": "tr/pages/double_play.html",
+            }
+    return render(request, "tr/base.html", context)
 
-def solo_play_view ( request ):
 
-	context = {
-		'title':'P4ngP2ong',
-		'lang':'en',
-        'username': 'clara',
-		'page':'tr/pages/solo_play.html',
-    }
-	return ( render( request, 'tr/base.html', context ) )
+def solo_play_view(request):
 
-def play_view ( request ):
     context = {
-		"title":"P4ngP2ong",
-		"lang":"en",
-        "username": "clara",
-		'page': 'tr/pages/play.html',
-    }
-    return ( render ( request, 'tr/base.html', context ) )
+            "title": "P4ngP2ong",
+            "lang": "en",
+            "username": "clara",
+            "page": "tr/pages/solo_play.html",
+            }
+    return render(request, "tr/base.html", context)
 
-def pong_view ( request ):
+
+def play_view(request):
     context = {
-		"title":"P4ngP2ong",
-		"lang":"en",
-        "username": "clara",
-		'page': "tr/pages/pong.html",
-    }
-    return ( render( request, 'tr/base.html', context ) )
+            "title": "P4ngP2ong",
+            "lang": "en",
+            "username": "clara",
+            "page": "tr/pages/play.html",
+            }
+    return render(request, "tr/base.html", context)
+
+
+def pong_view(request):
+    context = {
+            "title": "P4ngP2ong",
+            "lang": "en",
+            "username": "clara",
+            "page": "tr/pages/pong.html",
+            }
+    return render(request, "tr/base.html", context)
